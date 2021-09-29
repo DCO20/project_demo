76245c72d9d8597bf2e1f1da9090c4e93d40a950
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-09-29 14:16:48 -0300

    Migrate do module country

---
+++ Modules/Country/Database/Migrations/2021_09_30_140917_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('location_id');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
