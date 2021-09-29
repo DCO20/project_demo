86a637ec25cce4abbf8252d414b0377274fd4584
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-09-29 15:20:14 -0300

    Ajustes views do module country

---
+++ Modules/Country/Resources/views/index.blade.php
@@ -0,0 +1,43 @@
+@extends('country::layouts.master')
+
+@section('title', 'Paises')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+                    <div class="mt-3 ml-3">
+                        <a href="{{ route('countries.create') }}" class="btn btn-primary">
+                            <i class="fa fa-plus"></i> Novo</a>
+                        </a>
+                    </div>
+                    <div class="card-header">
+                        <h3 class="card-title">Paises</h3>
+                    </div>
+
+                    @include('country::includes.alerts')
+
+                    <!-- /.card-header -->
+                    <div class="card-body">
+
+                        <input type="hidden" id="route_datatable_country" value="">
+
+                        <table class="table table-bordered table-striped" id="ajax-datatable-country">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
