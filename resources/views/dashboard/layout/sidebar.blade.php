d0609d547c599a0375c48055dcc6ea16554a496b
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-09-29 15:18:40 -0300

    ajustes sidebar

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <span class="brand-text font-weight-light ml-5"><strong>Demons</strong> trativo</span>
+      <span class="brand-text font-weight-light ml-5"><strong>Demons</strong>trativo</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,9 +11,9 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="" class="nav-link {{ Ekko::isActiveRoute('')}}">
-              <i class="fas fa-chalkboard-teacher"></i>
-              <p></p>
+            <a href="{{ route('countries.index') }}" class="nav-link {{ Ekko::isActiveRoute('countries*')}}">
+              <i class="fas fa-globe-americas"></i>
+              <p>Paises</p>
             </a>
           </li>
         </ul>
