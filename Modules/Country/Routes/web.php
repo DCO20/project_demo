028f08914dedebe1ebc06eff0a9dd976f6e6e4f4
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-09-29 15:19:14 -0300

    rotas country

--- Modules/Country/Routes/web.php
+++ Modules/Country/Routes/web.php
@@ -1,2 +1,18 @@
 <?php
 
+use Modules\Country\Http\Controllers\CountryController;
+
+Route::group([
+    'prefix' => 'countries',
+    'as' => 'countries.'
+], function () {
+
+    Route::get('/datatable', [CountryController::class, 'dataTable'])->name('datatable');
+    Route::delete('/destroy/{id}', [CountryController::class, 'destroy'])->name('destroy');
+    Route::put('/edit/{id}', [CountryController::class, 'update'])->name('update');
+    Route::get('/edit/{id}', [CountryController::class, 'edit'])->name('edit');
+    Route::get('/show/{id}', [CountryController::class, 'show'])->name('show');
+    Route::post('/create', [CountryController::class, 'store'])->name('store');
+    Route::get('/create', [CountryController::class, 'create'])->name('create');
+    Route::get('/', [CountryController::class, 'index'])->name('index');
+});
