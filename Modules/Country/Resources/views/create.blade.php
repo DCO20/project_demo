4ba8bd6c5d5babab0615f614f27036e2880edbec
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-09-30 10:42:16 -0300

    ajustes na create country

--- Modules/Country/Resources/views/create.blade.php
+++ Modules/Country/Resources/views/create.blade.php
@@ -27,7 +27,7 @@
                             <div class="row ">
                                 <div class="col-md-12">
                                     <div class="form-group">
-                                        <label>Nome <span class="text-danger">*</span></label>
+                                        <label>Nome: <span class="text-danger">*</span></label>
                                         <input type="text" name="name" class="form-control" required>
                                     </div>
                                 </div>
