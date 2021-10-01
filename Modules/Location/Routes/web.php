677b7c6eef76ab46c3ee88e764f4daebe8c5a68b
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-01 16:45:48 -0300

    rotas da localização

--- Modules/Location/Routes/web.php
+++ Modules/Location/Routes/web.php
@@ -1,1 +1,36 @@
 <?php
+
+use Modules\Location\Http\Controllers\LocationController;
+
+Route::group([
+    'prefix' => 'dashboard/location',
+    'as' => 'location.'
+], function () {
+
+    Route::get('/', [LocationController::class, 'index'])
+        ->name('index');
+
+    Route::post('/datatable', [LocationController::class, 'dataTable'])
+        ->name('datatable');
+
+    Route::get('/{id}/ver', [LocationController::class, 'show'])
+        ->name('show');
+
+    Route::get('/cadastrar', [LocationController::class, 'create'])
+        ->name('create');
+
+    Route::post('/cadastrar', [LocationController::class, 'store'])
+        ->name('store');
+
+    Route::get('/{id}/editar', [LocationController::class, 'edit'])
+        ->name('edit');
+
+    Route::put('/{id}/editar', [LocationController::class, 'update'])
+        ->name('update');
+
+    Route::get('/{id}/confirmar-exclusao', [LocationController::class, 'confirmDelete'])
+        ->name('confirm_delete');
+
+    Route::delete('/{id}/excluir', [LocationController::class, 'delete'])
+        ->name('delete');
+});
