3932f4d8a0b4d99bf28f7f0e93350e1b9307da1d
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-02 07:08:57 -0300

    location js

---
+++ Modules/Location/Resources/assets/js/location.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    //Initialize Select2 Elements
+    $(".select2").select2();
+
+    //-----------------------------------------------------
+    // Defining a variable
+    //-----------------------------------------------------
+    var token = $("input[name='_token']").val();
+    var datatable_url = window.location.origin + "/datatable/pt-br.json";
+
+    //-----------------------------------------------------
+    // Instance of plugins
+    //-----------------------------------------------------
+    $("#ajax-datatable_location").DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: $("#route_datatable_location").val(),
+            type: "POST",
+            beforeSend: function (request) {
+                return request.setRequestHeader("X-CSRF-Token", token);
+            },
+        },
+        columns: [
+            { data: "id" },
+            { data: "lat" },
+            { data: "log" },
+            { data: "country" },
+            { data: "action", orderable: false, searchable: false },
+        ],
+        language: {
+            url: datatable_url,
+        },
+    });
+});
