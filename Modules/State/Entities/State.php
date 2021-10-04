a3c7f5d17f752f38693604e8591e84b139755c83
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-04 14:46:59 -0300

    Entidade do mudulo estados

---
+++ Modules/State/Entities/State.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\State\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class State extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'states';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'country_id',
+        'name',
+        'initial'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+}
