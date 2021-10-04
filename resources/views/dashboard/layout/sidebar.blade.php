426b24c1ebb728847c7f28252797b2ade3c5385d
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-04 11:28:06 -0300

    ajustes sidebar

--- resources/views/dashboard/layout/sidebar.blade.php
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -11,38 +11,16 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="fas fa-dice-one"></i>
-                        <p>
-                            One to One
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
+                    <a href="{{ route('country.index') }}" class="nav-link {{ Ekko::isActiveRoute('country*') }}">
+                        <i class="fas fa-globe-americas"></i>
+                        <p>Paises</p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('country.index') }}" class="nav-link {{ Ekko::isActiveRoute('country*') }}">
-                                <i class="fas fa-globe-americas"></i>
-                                <p>Paises</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('location.index') }}" class="nav-link {{ Ekko::isActiveRoute('location*') }}">
-                                <i class="fas fa-location-arrow"></i>
-                                <p>Localização</p>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
-                       <i class="fab fa-slack-hash"></i>
-                        <p>
-                           Has Many
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
+                    <a href="{{ route('location.index') }}" class="nav-link {{ Ekko::isActiveRoute('location*') }}">
+                        <i class="fas fa-location-arrow"></i>
+                        <p>Localização</p>
                     </a>
-                    <ul class="nav nav-treeview">
-                    </ul>
                 </li>
             </ul>
         </nav>
