257c989caa702579518912a4056247c87367b6d3
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-05 14:07:42 -0300

    Entidade do module product

---
+++ Modules/Product/Entities/Product.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Modules\Product\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'products';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'name',
+        'active',
+        'price',
+        'description'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+
+    /**
+     * Trativa da tabela do banco de dados
+     *
+     *  @var array $casts
+     */
+    protected $casts = [
+        'active' => 'boolean',
+        'price' => 'double'
+    ];
+
+    /**
+     * Retorna sim ou não
+     *
+     */
+    public function getFormattedActiveAttribute()
+    {
+        return $this->active ? "Sim" : "Não";
+    }
+
+
+    /**
+     * Leva para o banco de dados a entrada convertida para 00.00
+     *
+     */
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = str_replace(',', '.', str_replace('.', '', $value));
+    }
+
+
+    /**
+     * Trás do banco de dados a entrada convertida para 100,00
+     *
+     */
+    public function getFormattedPriceAttribute()
+    {
+        return number_format($this->attributes['price'], 2, ',', '.');
+    }
+}
