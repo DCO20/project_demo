37257eb1373f891c113ed9ed604874712ad7694c
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-06 16:45:43 -0300

    Rotas do module provider

--- Modules/Provider/Routes/web.php
+++ Modules/Provider/Routes/web.php
@@ -1,12 +1,36 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Modules\Provider\Http\Controllers\ProviderController;
+
+Route::group([
+    'prefix' => 'dashboard/provider',
+    'as' => 'provider.'
+], function () {
+
+    Route::get('/', [ProviderController::class, 'index'])
+        ->name('index');
+
+    Route::post('/datatable', [ProviderController::class, 'dataTable'])
+        ->name('datatable');
+
+    Route::get('/{id}/ver', [ProviderController::class, 'show'])
+        ->name('show');
+
+    Route::get('/cadastrar', [ProviderController::class, 'create'])
+        ->name('create');
+
+    Route::post('/cadastrar', [ProviderController::class, 'store'])
+        ->name('store');
+
+    Route::get('/{id}/editar', [ProviderController::class, 'edit'])
+        ->name('edit');
+
+    Route::put('/{id}/editar', [ProviderController::class, 'update'])
+        ->name('update');
+
+    Route::get('/{id}/confirmar-exclusao', [ProviderController::class, 'confirmDelete'])
+        ->name('confirm_delete');
+
+    Route::delete('/{id}/excluir', [ProviderController::class, 'delete'])
+        ->name('delete');
+});
