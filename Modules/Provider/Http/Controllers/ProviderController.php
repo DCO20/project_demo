12614b6d1b11bf4d9a4c7fcbb9e1fc26f824fff3
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-06 16:44:17 -0300

    Controller do module provider

--- Modules/Provider/Http/Controllers/ProviderController.php
+++ Modules/Provider/Http/Controllers/ProviderController.php
@@ -2,15 +2,36 @@
 
 namespace Modules\Provider\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 use Illuminate\Routing\Controller;
+use Modules\Provider\Entities\Provider;
+use Modules\Provider\Services\ProviderService;
+use Modules\Provider\Http\Requests\ProviderRequest;
 
 class ProviderController extends Controller
 {
+    protected $provider;
+
+    protected $provider_service;
+
     /**
-     * Display a listing of the resource.
-     * @return Renderable
+     * Método Construtor
+     *
+     * @param provider $provider
+     * @return void
+     */
+    public function __construct(
+        Provider $provider,
+        ProviderService $provider_service
+    ) {
+        $this->provider = $provider;
+        $this->provider_service = $provider_service;
+    }
+
+    /**
+     * Exibe a tela inicial com a listagem de dados.
+     *
+     * @return \Illuminate\View\View
      */
     public function index()
     {
@@ -18,8 +39,30 @@ class ProviderController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     * @return Renderable
+     * Obtêm os dados para a tabela
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function dataTable()
+    {
+        $categories = $this->provider->query();
+
+        return DataTables::of($categories)
+            ->editColumn("active", function ($provider) {
+                return $provider->formatted_active;
+            })
+            ->addColumn("action", function ($provider) {
+                return $provider->actionView();
+            })
+            ->rawColumns(['active', 'description', 'action'])
+            ->make(true);
+    }
+
+    /**
+     * Exibe a tela de cadastro
+     *
+     * @return \Illuminate\View\View
      */
     public function create()
     {
@@ -27,53 +70,91 @@ class ProviderController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
+     * Cadastra e retorna para a tela inicial
+     *
+     * @param Requests\ProviderRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(ProviderRequest $request)
     {
-        //
+        $this->provider_service->updateOrCreate($request->all());
+
+        return redirect()
+            ->route('provider.index')
+            ->with('message', 'Cadastro realizado com sucesso.');
     }
 
     /**
-     * Show the specified resource.
+     * Exibe os dados
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        return view('provider::show');
+        $provider = $this->provider->findOrFail($id);
+
+        return view('provider::show', compact('provider'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Exibe os dados para edição
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        return view('provider::edit');
+        $provider = $this->provider->findOrFail($id);
+
+        return view('provider::edit', compact('provider'));
     }
 
     /**
-     * Update the specified resource in storage.
-     * @param Request $request
+     * Atualiza e retorna para a tela de edição
+     *
+     * @param Requests\ProviderRequest $request
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(ProviderRequest $request, $id)
     {
-        //
+        $provider = $this->provider->findOrFail($id);
+
+        $this->provider_service->updateOrCreate($request->all(), $provider->id);
+
+        return redirect()
+            ->route('provider.edit', $provider->id)
+            ->with('message', 'Atualização realizada com sucesso.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Exibe a tela para exclusão
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
-    public function destroy($id)
+    public function confirmDelete($id)
     {
-        //
+        $provider = $this->provider->findOrFail($id);
+
+        return view('provider::confirm-delete', compact('provider'));
+    }
+
+    /**
+     * Exclui e retorna para a tela inicial
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $provider = $this->provider->findOrFail($id);
+
+        $this->provider_service->removeData($provider);
+
+        return redirect()
+            ->route('provider.index')
+            ->with('message', 'Exclusão realizada com sucesso.');
     }
 }
