eb339d007fd6f1ae72b9bf00db65fd5506cedc0c
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-07 09:09:09 -0300

    tabela addresses do module provider

---
+++ Modules/Provider/Database/Migrations/2021_10_07_090422_create_adresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('provider_id');
+            $table->string('zipcode');
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement')->nullable();
+            $table->string('district');
+            $table->string('ref_point')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('provider_id')->references('id')->on('providers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adresses');
+    }
+}
