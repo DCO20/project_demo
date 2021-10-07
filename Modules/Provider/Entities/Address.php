df934ea465021a6d2733a879909669257abefbbe
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-07 09:15:34 -0300

    Entidade address do module provider

---
+++ Modules/Provider/Entities/Address.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Modules\Provider\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Address extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = "addresses";
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'provider_id',
+        'zipcode',
+        'street',
+        'number',
+        'complement',
+        'district',
+        'ref_point'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
