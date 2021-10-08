6895ebd9b564608c06a289c025c6e2ea98023274
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-08 09:40:32 -0300

    Ajustes no  module provider

--- Modules/Provider/Resources/views/edit.blade.php
+++ Modules/Provider/Resources/views/edit.blade.php
@@ -61,7 +61,7 @@
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label>Razão social:<span class="text-danger">*</span></label>
-                                            <input type="text" name="legal_name" class="form-control" required value="{{ $provider->legal_name }}">
+                                            <input type="text" name="legal_name" id="legal_name" class="form-control" required value="{{ $provider->legal_name }}">
                                         </div>
                                     </div>
 
@@ -70,7 +70,7 @@
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label>Nome Fantasia:<span class="text-danger">*</span></label>
-                                            <input type="text" name="trade_name" class="form-control" required value="{{ $provider->trade_name }}">
+                                            <input type="text" name="trade_name"  id="trade_name" class="form-control" required value="{{ $provider->trade_name }}">
                                         </div>
                                     </div>
 
