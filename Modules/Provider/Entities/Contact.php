89d2a5c45303374dc63a8e63cc8e00ce17fad46a
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-08 10:48:04 -0300

    Entidade contato do fornecedor

---
+++ Modules/Provider/Entities/Contact.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\Provider\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = "contacts";
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'provider_id',
+        'phone'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Relacionamento para o fornecedor
+     *
+     *  @var array
+     */
+    public function provider()
+    {
+        return $this->belongsTo(Provider::class);
+    }
+}
