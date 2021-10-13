86c86a9357b38252ffd6b1c5816c028cf340e751
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 15:01:13 -0300

    Criação da entidade do module product

---
+++ Modules/Product/Entities/Product.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Modules\Product\Entities;
+
+use App\Traits\Presentable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Product\Presenter\ProductPresenter;
+
+class Product extends Model
+{
+    use SoftDeletes,
+        Presentable;
+
+    /**
+     * Presenter
+     *
+     * @var string $presenter
+     */
+    protected $presenter = ProductPresenter::class;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'products';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'name',
+        'active',
+        'price'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+
+    /**
+     * Trativa da tabela do banco de dados
+     *
+     *  @var array $casts
+     */
+    protected $casts = [
+        'active' => 'boolean',
+        'price' => 'float'
+    ];
+
+    /**
+     * Retorna sim ou não
+     *
+     */
+    public function getFormattedActiveAttribute()
+    {
+        return $this->active ? "Sim" : "Não";
+    }
+
+
+    /**
+     * Formata o atributo
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = str_replace(',', '.', str_replace('.', '', $value));
+    }
+
+
+    /**
+     * Formata o atributo
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getFormattedPriceAttribute()
+    {
+        return number_format($this->attributes['price'], 2, ',', '.');
+    }
+}
