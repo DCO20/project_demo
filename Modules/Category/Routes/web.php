db26b65d24327d972b4aec5a230fadf2ce9d6096
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 14:37:36 -0300

    Ajustes nas rotas do module category

--- Modules/Category/Routes/web.php
+++ Modules/Category/Routes/web.php
@@ -1,16 +1,36 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Modules\Category\Http\Controllers\CategoryController;
 
-Route::prefix('category')->group(function() {
-    Route::get('/', 'CategoryController@index');
+Route::group([
+    'prefix' => 'dashboard/category',
+    'as' => 'category.'
+], function () {
+
+    Route::get('/', [CategoryController::class, 'index'])
+        ->name('index');
+
+    Route::post('/datatable', [CategoryController::class, 'dataTable'])
+        ->name('datatable');
+
+    Route::get('/{id}/ver', [CategoryController::class, 'show'])
+        ->name('show');
+
+    Route::get('/cadastrar', [CategoryController::class, 'create'])
+        ->name('create');
+
+    Route::post('/cadastrar', [CategoryController::class, 'store'])
+        ->name('store');
+
+    Route::get('/{id}/editar', [CategoryController::class, 'edit'])
+        ->name('edit');
+
+    Route::put('/{id}/editar', [CategoryController::class, 'update'])
+        ->name('update');
+
+    Route::get('/{id}/confirmar-exclusao', [CategoryController::class, 'confirmDelete'])
+        ->name('confirm_delete');
+
+    Route::delete('/{id}/excluir', [CategoryController::class, 'delete'])
+        ->name('delete');
 });
