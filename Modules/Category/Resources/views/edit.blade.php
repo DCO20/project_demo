4a2d0a903b854ed8d7fb798e757f0507e81b4f98
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 14:39:15 -0300

    Criação das views do module category

---
+++ Modules/Category/Resources/views/edit.blade.php
@@ -0,0 +1,102 @@
+@extends('category::layouts.master')
+
+@section('title', 'Categorias')
+
+@section('content_header')
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-10">
+                <ol class="breadcrumb float-sm-left">
+                    <li class="breadcrumb-item">Categorias</li>
+                    <li class="breadcrumb-item active">Editar</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+
+    {{-- Respostas --}}
+    @include('dashboard.partials.errors')
+    @include('dashboard.partials.success')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{ route('category.update', $category->id) }}" method="post">
+
+                    {{-- Elementos Ocultos --}}
+                    @csrf
+                    @method('PUT')
+
+                    <div class="card card-outline card-secondary">
+
+                        <div class="card-header">
+                            <h3 class="card-title">
+                                Dados da Estados
+                            </h3>
+                        </div>
+
+                        <div class="card-body">
+
+                            <div class="row">
+
+                                {{-- Nome --}}
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Nome:<span class="text-danger">*</span></label>
+                                        <input type="text" name="name" class="form-control" value="{{ $category->name }}" required>
+                                    </div>
+                                </div>
+
+                                {{-- Ativo --}}
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Ativo:<span class="text-danger">*</span></label>
+                                        <select name="active" class="form-control select2" style="width: 100%;" required>
+                                            <option value="1" @if($category->active) selected @endif >Sim</option>
+                                            <option value="0" @if(!$category->active)  selected @endif >Não</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            {{-- Descrição --}}
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Descrição:</label>
+                                        <textarea name="description" id="summernote" cols="50" rows="5" class="form-control">{{ $category->description }}</textarea>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="card-footer"></div>
+
+                    </div>
+
+                    {{-- Botão que salva os dados --}}
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-plus fa-fw"></i> Salvar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('footer-extras')
+    <script src="{{ mix('js/category.js') }}"></script>
+@endsection
