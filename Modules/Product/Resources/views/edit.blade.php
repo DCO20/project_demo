b66daa19a7a09ccd283f344438e9ef6007f417dc
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 15:18:57 -0300

    Criação das views do module category

---
+++ Modules/Product/Resources/views/edit.blade.php
@@ -0,0 +1,101 @@
+@extends('product::layouts.master')
+
+@section('title', 'Produtos')
+
+@section('content_header')
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-10">
+                <ol class="breadcrumb float-sm-left">
+                    <li class="breadcrumb-item">Produtos</li>
+                    <li class="breadcrumb-item active">Editar</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+
+    {{-- Respostas --}}
+    @include('dashboard.partials.errors')
+    @include('dashboard.partials.success')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{ route('product.update', $product->id) }}" method="post">
+
+                    {{-- Elementos Ocultos --}}
+                    @csrf
+                    @method('PUT')
+
+                    <div class="card card-outline card-secondary">
+
+                        <div class="card-header">
+                            <h3 class="card-title">
+                                Dados da Estados
+                            </h3>
+                        </div>
+
+                        <div class="card-body">
+
+                            <div class="row">
+
+                                {{-- Nome --}}
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Nome:<span class="text-danger">*</span></label>
+                                        <input type="text" name="name" class="form-control" required value="{{ $product->name }}">
+                                    </div>
+                                </div>
+
+                                 {{-- Preço --}}
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Preço:<span class="text-danger">*</span></label>
+                                        <input type="text" name="price" class="form-control money" required value="{{ $product->formatted_price }}">
+                                    </div>
+                                </div>
+
+                                {{-- Ativo --}}
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label>Ativo:<span class="text-danger">*</span></label>
+                                        <select name="active" class="form-control select2" style="width: 100%;" required>
+
+                                            <option value="1" @if ($product->active) selected @endif>Sim</option>
+                                            <option value="0" @if (!$product->active)  selected @endif>Não</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="card-footer"></div>
+
+                    </div>
+
+                    {{-- Botão que salva os dados --}}
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success">
+                                    <i class="fa fa-plus fa-fw"></i> Salvar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('footer-extras')
+    <script src="{{ mix('js/product.js') }}"></script>
+@endsection
--- Modules/Product/Resources/views/index.blade.php
+++ Modules/Product/Resources/views/index.blade.php
@@ -1,9 +1,74 @@
 @extends('product::layouts.master')
 
-@section('content')
-    <h1>Hello World</h1>
+@section('title', 'Produtos')
 
-    <p>
-        This view is loaded from module: {!! config('product.name') !!}
-    </p>
+@section('header-extras')
+@endsection
+
+@section('content_header')
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-10">
+                <ol class="breadcrumb float-sm-left">
+                    <li class="breadcrumb-item">Produtos</li>
+                    <li class="breadcrumb-item active">Painel de Controle</li>
+                </ol>
+            </div>
+            <div class="col-sm-2 text-right">
+                <a href="{{ route('product.create') }}" class="btn btn-primary">
+                    <i class="fa fa-plus fa-fw"></i> Cadastrar
+                </a>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+
+    {{-- Respostas --}}
+    @include('dashboard.partials.errors')
+    @include('dashboard.partials.success')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+
+                <div class="card">
+
+                    <div class="card-body">
+
+                        {{-- Elementos Ocultos --}}
+                        @csrf
+                        <input type="hidden" id="route_datatable" value="{{ route('product.datatable') }}">
+
+                        <table class="table table-bordered table-striped" id="ajax-datatable">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Ativo</th>
+                                    <th>Preço</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nome</th>
+                                    <th>Ativo</th>
+                                    <th>Preço</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('footer-extras')
+    <script src="{{ mix('js/product.js') }}"></script>
 @endsection
