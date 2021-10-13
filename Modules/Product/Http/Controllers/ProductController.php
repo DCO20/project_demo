9a7288dc1b5979c7c6a2b100742796824b257af7
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 15:09:43 -0300

    Ajustes do controller do module product

--- Modules/Product/Http/Controllers/ProductController.php
+++ Modules/Product/Http/Controllers/ProductController.php
@@ -2,15 +2,37 @@
 
 namespace Modules\Product\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
 use Illuminate\Routing\Controller;
+use Modules\Product\Entities\Product;
+use Modules\Category\Entities\Category;
+use Modules\Product\Services\ProductService;
+use Modules\Product\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
+    protected $product;
+
+    protected $product_service;
+
     /**
-     * Display a listing of the resource.
-     * @return Renderable
+     * Método Construtor
+     *
+     * @param product $product
+     * @return void
+     */
+    public function __construct(
+        Product $product,
+        ProductService $product_service
+    ) {
+        $this->product = $product;
+        $this->product_service = $product_service;
+    }
+
+    /**
+     * Exibe a tela inicial com a listagem de dados.
+     *
+     * @return \Illuminate\View\View
      */
     public function index()
     {
@@ -18,62 +40,135 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     * @return Renderable
+     * Obtêm os dados para a tabela
+     * @codeCoverageIgnore
+     *
+     * @return string
+     */
+    public function dataTable()
+    {
+        $products = $this->product->query();
+
+        return DataTables::of($products)
+            ->editColumn("active", function ($product) {
+                return $product->formatted_active;
+            })
+            ->editColumn("price", function ($product) {
+                return $product->formatted_price;
+            })
+            ->filterColumn(
+                'price',
+                function ($q, $keyword) {
+                    $formatted_price = str_replace(',', '.', str_replace('.', '', $keyword));
+
+                    $q->where('price', 'LIKE', '%' . $formatted_price . '%');
+                }
+            )
+            ->addColumn("action", function ($product) {
+                return $product->actionView();
+            })
+            ->rawColumns(['active', 'action'])
+            ->make(true);
+    }
+
+    /**
+     * Exibe a tela de cadastro
+     *
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        return view('product::create');
+        $categories = Category::where('active', true)->get();
+
+        return view('product::create', compact('categories'));
     }
 
     /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
+     * Cadastra e retorna para a tela inicial
+     *
+     * @param Requests\ProductRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $this->product_service->updateOrCreate($request->all());
+
+        return redirect()
+            ->route('product.index')
+            ->with('message', 'Cadastro realizado com sucesso.');
     }
 
     /**
-     * Show the specified resource.
+     * Exibe os dados
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        return view('product::show');
+        $product = $this->product->findOrFail($id);
+
+        return view('product::show', compact('product'));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Exibe os dados para edição
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        return view('product::edit');
+        $product = $this->product->findOrFail($id);
+
+        return view('product::edit', compact('product', 'categories'));
     }
 
     /**
-     * Update the specified resource in storage.
-     * @param Request $request
+     * Atualiza e retorna para a tela de edição
+     *
+     * @param Requests\ProductRequest $request
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
-        //
+        $product = $this->product->findOrFail($id);
+
+        $this->product_service->updateOrCreate($request->all(), $product->id);
+
+        return redirect()
+            ->route('product.edit', $product->id)
+            ->with('message', 'Atualização realizada com sucesso.');
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Exibe a tela para exclusão
+     *
      * @param int $id
-     * @return Renderable
+     * @return \Illuminate\View\View
      */
-    public function destroy($id)
+    public function confirmDelete($id)
     {
-        //
+        $product = $this->product->findOrFail($id);
+
+        return view('product::confirm-delete', compact('product'));
+    }
+
+    /**
+     * Exclui e retorna para a tela inicial
+     *
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $product = $this->product->findOrFail($id);
+
+        $this->product_service->removeData($product);
+
+        return redirect()
+            ->route('product.index')
+            ->with('message', 'Exclusão realizada com sucesso.');
     }
 }
