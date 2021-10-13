88544cafd9e6259fc6afdcc2cfceca50f9a43076
Danilo Oliveira <61196426+DCO20@users.noreply.github.com>
2021-10-13 14:31:08 -0300

    Criação da entidade do module category

---
+++ Modules/Category/Entities/Category.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Modules\Category\Entities;
+
+use App\Traits\Presentable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Category\Presenter\CategoryPresenter;
+
+class Category extends Model
+{
+    use SoftDeletes,
+        Presentable;
+
+    /**
+     * Presenter
+     *
+     * @var string $presenter
+     */
+    protected $presenter = CategoryPresenter::class;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string $table
+     */
+    protected $table = 'categories';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $fillable
+     */
+    protected $fillable = [
+        'name',
+        'active'
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array $dates
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+
+    /**
+     * Trativa da tabela do banco de dados
+     *
+     *  @var array $casts
+     */
+    protected $casts = [
+        'active' => 'boolean'
+    ];
+
+    /**
+     * Formata o atributo
+     *
+     * @param string $value
+     * @return boolean
+     */
+    public function getFormattedActiveAttribute()
+    {
+        return $this->active ? "Sim" : "Não";
+    }
+}
