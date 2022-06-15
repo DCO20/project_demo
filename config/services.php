878c5a5c289234a812b574c126c1f79f1005ed4c
@danilo.cardoso <danilo.cardoso@vercan.com.br>
2022-06-15 14:34:12 -0300

    correção service config

--- config/services.php
+++ config/services.php
@@ -33,19 +33,19 @@ return [
 	'google' => [
 		'client_id' => env('GOOGLE_CLIENT_ID'),
 		'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-		'redirect' => 'http://project_demo.local/login/google/callback',
+		'redirect' => env('APP_URL') .'login/google/callback',
 	],
 
 	'facebook' => [
 		'client_id' => env('FACEBOOK_CLIENT_ID'),
 		'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-		'redirect' => 'http://project_demo.local/login/facebook/callback',
+		'redirect' => env('APP_URL') . 'login/facebook/callback',
 	],
 
 	'github' => [
 		'client_id' => env('GITHUB_CLIENT_ID'),
 		'client_secret' => env('GITHUB_CLIENT_SECRET'),
-		'redirect' => 'http://project_demo.local/login/github/callback',
+		'redirect' => env('APP_URL') . 'login/github/callback',
 	],
 
 ];
