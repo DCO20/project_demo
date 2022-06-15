c6d66515c4c5cdd8f8e2256d0f9729127e02c81a
@danilo.cardoso <danilo.cardoso@vercan.com.br>
2022-06-15 08:58:12 -0300

    correcções

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('/', function () {
+	return redirect()->route('client.index');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
